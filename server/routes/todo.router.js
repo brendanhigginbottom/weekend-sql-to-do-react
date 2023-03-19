4dd3b18ef21fec81add2c6b9bcd9fb3e13528243
Brendan Higginbottom <brendan.higginbottom@gmail.com>
2023-03-19 12:15:41 -0500

    test

--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -3,8 +3,31 @@ const router = express.Router();
 const pool = require('../modules/pool.js');
 
 // GET
+router.get('/', (req, res) => {
+    console.log('In GET Request /todo');
+    let queryText = '';
+
+    pool.query(queryText).then((result) => {
+        console.log(`Testing for result: ${result}`);
+        res.send(result.rows);
+    }).catch((error) => {
+        console.log(`Error in GET ${error}`);
+        res.sendStatus(500);
+    });
+});
 
 // POST
+router.post('/', (req, res) => {
+    console.log(`/todo POST Request made: ${req.body}`);
+    let queryText = '';
+    pool.query(queryText, [])
+    .then((result) => {
+        res.sendStatus(200);
+    }).catch((error) => {
+        console.log(`Error in POST ${error}`);
+        res.sendStatus(500);
+    })
+})
 
 // PUT
 
