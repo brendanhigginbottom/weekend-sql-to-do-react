e5998a72135b861139d700e2d8de714435840d3e
Brendan Higginbottom <brendan.higginbottom@gmail.com>
2023-03-26 13:46:56 -0500

    moved table to its own component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -24,23 +24,6 @@ function App () {
     fetchToDo();
   }, []);
 
-  // const submitForm = (event) => {
-  //   event.preventDefault();
-
-  //   axios.post('/todo', {
-  //     task_name: taskName,
-  //     task_desc: taskDesc
-  //   }).then((response) => {
-  //     setTaskName('');
-  //     setTaskDesc('');
-  //     fetchToDo();
-  //     console.log('In submitForm');
-  //   }).catch((error) => {
-  //     console.log(`Error in POST ${error}`);
-  //     alert('Something went wrong');
-  //   });
-  // }
-
   return (
     <div>
       <h1>TO DO APP</h1>
@@ -51,40 +34,9 @@ function App () {
         setTaskDesc={setTaskDesc}
         fetchToDo={fetchToDo}
       />
-      {/* <form onSubmit={submitForm}>
-        Task: <input type="text" value={taskName}
-          onChange={(e) => setTaskName(e.target.value)} />
-        Task Description: <input type="text" value={taskDesc}
-          onChange={(e) => setTaskDesc(e.target.value)} />
-        <input type="submit" value="Add Task" />
-      </form> */}
-      <table>
-        <thead>
-          <tr>
-            <th>Date Added</th>
-            <th>Task</th>
-            <th>Task Description</th>
-            <th>Completed</th>
-            <th></th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            toDoList.map((task) => (
-              <tr key={task.id}>
-                {/* Using substring to truncate time to just date */}
-                <td>{task.date.substring(0,10)}</td>
-                <td>{task.task_name}</td>
-                <td>{task.task_desc}</td>
-                <td>{task.complete.toString()}</td>
-                <td><button>Complete</button></td>
-                <td><button>Delete</button></td>
-              </tr>
-            ))
-          }
-        </tbody>
-      </table>
+      <ToDoList 
+        toDoList={toDoList}
+      />
     </div>
   );
 }
--- src/components/ToDoList/ToDoList.jsx
+++ src/components/ToDoList/ToDoList.jsx
@@ -1,10 +1,36 @@
 
 
-function ToDoList() {
+function ToDoList({ toDoList }) {
 
     return (
         <>
-        
+            <table>
+                <thead>
+                    <tr>
+                        <th>Date Added</th>
+                        <th>Task</th>
+                        <th>Task Description</th>
+                        <th>Completed</th>
+                        <th></th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                {
+                    toDoList.map((task) => (
+                    <tr key={task.id}>
+                        {/* Using substring to truncate time to just date */}
+                        <td>{task.date.substring(0,10)}</td>
+                        <td>{task.task_name}</td>
+                        <td>{task.task_desc}</td>
+                        <td>{task.complete.toString()}</td>
+                        <td><button>Complete</button></td>
+                        <td><button>Delete</button></td>
+                    </tr>
+                    ))
+                }
+                </tbody>
+            </table>
         </>
     );
 }
